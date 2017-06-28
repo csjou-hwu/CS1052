f18a6868866cf3737851493bcf794bfd895b5fe1
csjou.hwu@gmail.com <csjou.hwu@gmail.com>
2017-06-28 17:55:29 +0800

    upload wk17

---
+++ wk17/wk171/MainWindow.xaml.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wk171
+{
+    using System;
+    using System.Globalization;
+    using System.IO;
+    using System.Windows;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using Microsoft.Kinect;
+    using Microsoft.Kinect.Toolkit;
+    using Microsoft.Kinect.Toolkit.BackgroundRemoval;
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region // 宣告
+        /// <summary>
+        /// Format we will use for the depth stream
+        /// </summary>
+        private const DepthImageFormat DepthFormat = DepthImageFormat.Resolution320x240Fps30;
+
+        /// <summary>
+        /// Format we will use for the color stream
+        /// </summary>
+        private const ColorImageFormat ColorFormat = ColorImageFormat.RgbResolution640x480Fps30;
+
+        /// <summary>
+        /// Bitmap that will hold color information
+        /// </summary>
+        private WriteableBitmap foregroundBitmap;
+
+        /// <summary>
+        /// Active Kinect sensor
+        /// </summary>
+        // Modify Chooser
+        private KinectSensor sensorChooser;
+
+        /// <summary>
+        /// Our core library which does background 
+        /// </summary>
+        private BackgroundRemovedColorStream backgroundRemovedColorStream;
+
+        /// <summary>
+        /// Intermediate storage for the skeleton data received from the sensor
+        /// </summary>
+        private Skeleton[] skeletons;
+
+        /// <summary>
+        /// the skeleton that is currently tracked by the app
+        /// </summary>
+        private int currentlyTrackedSkeletonId;
+
+        /// <summary>
+        /// Track whether Dispose has been called
+        /// </summary>
+        private bool disposed;
+        #endregion // 
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            // initialize the sensor chooser and UI
+            this.sensorChooser = KinectSensor.KinectSensors[0];
+            //this.sensorChooser.KinectChanged += this.SensorChooserOnKinectChanged;
+            //--- Add
+            #region // Sensor 啟動
+            sensorChooser.DepthStream.Enable(DepthFormat);
+            sensorChooser.ColorStream.Enable(ColorFormat);
+            sensorChooser.SkeletonStream.Enable();
+
+            this.backgroundRemovedColorStream = new BackgroundRemovedColorStream(sensorChooser);
+            this.backgroundRemovedColorStream.Enable(ColorFormat, DepthFormat);
+
+            // Allocate space to put the depth, color, and skeleton data we'll receive
+            if (null == this.skeletons)
+            {
+                this.skeletons = new Skeleton[sensorChooser.SkeletonStream.FrameSkeletonArrayLength];
+            }
+
+            // Add an event handler to be called when the background removed color frame is ready, so that we can
+            // composite the image and output to the app
+            this.backgroundRemovedColorStream.BackgroundRemovedFrameReady += this.BackgroundRemovedFrameReadyHandler;
+
+            // Add an event handler to be called whenever there is new depth frame data
+            sensorChooser.AllFramesReady += this.SensorAllFramesReady;
+
+            try
+            {
+                sensorChooser.DepthStream.Range = DepthRange.Default;
+                sensorChooser.SkeletonStream.EnableTrackingInNearRange = true;
+            }
+            catch (InvalidOperationException)
+            {
+                // Non Kinect for Windows devices do not support Near mode, so reset back to default mode.
+                sensorChooser.DepthStream.Range = DepthRange.Default;
+                sensorChooser.SkeletonStream.EnableTrackingInNearRange = false;
+            }
+            #endregion
+            this.sensorChooser.Start();
+        }
+        private void SensorAllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            // in the middle of shutting down, or lingering events from previous sensor, do nothing here.
+            //if (null == this.sensorChooser || null == this.sensorChooser.Kinect || this.sensorChooser.Kinect != sender)
+            //{
+            //    return;
+            //}
+
+            try
+            {
+                using (var depthFrame = e.OpenDepthImageFrame())
+                {
+                    if (null != depthFrame)
+                    {
+                        this.backgroundRemovedColorStream.ProcessDepth(depthFrame.GetRawPixelData(), depthFrame.Timestamp);
+                    }
+                }
+
+                using (var colorFrame = e.OpenColorImageFrame())
+                {
+                    if (null != colorFrame)
+                    {
+                        this.backgroundRemovedColorStream.ProcessColor(colorFrame.GetRawPixelData(), colorFrame.Timestamp);
+                    }
+                }
+
+                using (var skeletonFrame = e.OpenSkeletonFrame())
+                {
+                    if (null != skeletonFrame)
+                    {
+                        skeletonFrame.CopySkeletonDataTo(this.skeletons);
+                        this.backgroundRemovedColorStream.ProcessSkeleton(this.skeletons, skeletonFrame.Timestamp);
+                    }
+                }
+
+                this.ChooseSkeleton();
+            }
+            catch (InvalidOperationException)
+            {
+                // Ignore the exception. 
+            }
+        }
+
+        /// <summary>
+        /// Handle the background removed color frame ready event. The frame obtained from the background removed
+        /// color stream is in RGBA format.
+        /// </summary>
+        /// <param name="sender">object that sends the event</param>
+        /// <param name="e">argument of the event</param>
+        private void BackgroundRemovedFrameReadyHandler(object sender, BackgroundRemovedColorFrameReadyEventArgs e)
+        {
+            using (var backgroundRemovedFrame = e.OpenBackgroundRemovedColorFrame())
+            {
+                if (backgroundRemovedFrame != null)
+                {
+                    if (null == this.foregroundBitmap || this.foregroundBitmap.PixelWidth != backgroundRemovedFrame.Width
+                        || this.foregroundBitmap.PixelHeight != backgroundRemovedFrame.Height)
+                    {
+                        this.foregroundBitmap = new WriteableBitmap(backgroundRemovedFrame.Width, backgroundRemovedFrame.Height, 96.0, 96.0, PixelFormats.Bgra32, null);
+
+                        // Set the image we display to point to the bitmap where we'll put the image data
+                        this.MaskedColor.Source = this.foregroundBitmap;
+                    }
+
+                    // Write the pixel data into our bitmap
+                    this.foregroundBitmap.WritePixels(
+                        new Int32Rect(0, 0, this.foregroundBitmap.PixelWidth, this.foregroundBitmap.PixelHeight),
+                        backgroundRemovedFrame.GetRawPixelData(),
+                        this.foregroundBitmap.PixelWidth * sizeof(int),
+                        0);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Use the sticky skeleton logic to choose a player that we want to set as foreground. This means if the app
+        /// is tracking a player already, we keep tracking the player until it leaves the sight of the camera, 
+        /// and then pick the closest player to be tracked as foreground.
+        /// </summary>
+        private void ChooseSkeleton()
+        {
+            var isTrackedSkeltonVisible = false;
+            var nearestDistance = float.MaxValue;
+            var nearestSkeleton = 0;
+
+            foreach (var skel in this.skeletons)
+            {
+                if (null == skel)
+                {
+                    continue;
+                }
+
+                if (skel.TrackingState != SkeletonTrackingState.Tracked)
+                {
+                    continue;
+                }
+
+                if (skel.TrackingId == this.currentlyTrackedSkeletonId)
+                {
+                    isTrackedSkeltonVisible = true;
+                    break;
+                }
+
+                if (skel.Position.Z < nearestDistance)
+                {
+                    nearestDistance = skel.Position.Z;
+                    nearestSkeleton = skel.TrackingId;
+                }
+            }
+
+            if (!isTrackedSkeltonVisible && nearestSkeleton != 0)
+            {
+                this.backgroundRemovedColorStream.SetTrackedPlayer(nearestSkeleton);
+                this.currentlyTrackedSkeletonId = nearestSkeleton;
+            }
+        }
+
+
+
+    }
+}
